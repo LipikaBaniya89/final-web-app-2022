a3e14740cd52b453f8eb2ebccf87aaabf58a649b
LipikaBaniya89 <lipikabania9@gmail.com>
2022-03-21 22:48:22 +0700

    Initial commit

--- app.js
+++ app.js
@@ -27,12 +27,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static(path.join(__dirname, "public", "react-quotation"))); 
+// app.use(express.static(path.join(__dirname, "public", "react-quotation"))); 
 
-app.get("/react-quotation/*", function (req, res) { 
-  res.sendFile(path.join(__dirname, "public", "react-quotation", "index.html")); 
 
-}); 
 
 // Plug routers
 app.use('/', indexRouter);
@@ -45,6 +42,11 @@ app.use('/quotations',quotationRouter);
 app.use(function(req, res, next) {
   next(createError(404));
 });
+
+app.get("/*", function (req, res) { 
+  res.sendFile(path.join(__dirname, "public", "index.html")); 
+
+}); 
 
 // error handler
 app.use(function(err, req, res, next) {
