f034ace7cc18522ea5846c9dfea5e5daf6ebdc56
Lipika Baniya <89762072+LipikaBaniya89@users.noreply.github.com>
2022-03-22 09:17:16 +0700

    Update quotation.js

--- routes/quotation.js
+++ routes/quotation.js
@@ -20,7 +20,7 @@ router.get("/", (req, res, next) => {
 router.post("/", (req, res, next) => {
   console.debug(req.body);
   const data = req.body;
-  const dateObj = new Date().toDateString();
+  const dateObj = new Date().toLocaleString();
 
   const newQuotation = new Quotation({
     item: data.item,
